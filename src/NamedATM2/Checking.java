821805e7f9f6afcdee0a380d16ab9f4ffbadc78a
Michael <Michael@Michael-PC.attlocal.net>
2015-12-16 11:00:05 -0600

    protected methods

---
+++ src/NamedATM2/Checking.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NamedATM2;
+
+/**
+ *
+ * @author ml4kb
+ */
+public class Checking extends Account{
+    
+    public Checking(int bal, int num, String name, String PIN)
+    {
+        super(bal, num, name, PIN);
+    }
+    
+    protected void getInterest()
+    {
+        int datediff = seconddate - firstdate;
+        rate = .05/365;
+        double ratetime = Math.pow(1+rate, datediff);
+                
+        if(secondyear - firstyear > 0) //allows for different years to be input
+        {
+            int yeardiff = secondyear - firstyear;
+            ratetime *= Math.pow(1.05, yeardiff);
+        }
+        balance *= ratetime;
+        firstdate = seconddate;
+        firstyear = secondyear;
+    }
+}
